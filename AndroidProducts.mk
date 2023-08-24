c7fcc562c75f47cb980bdc2c908d045961313cfc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-08-24 14:06:42 +0200

    a03s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A037F Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a03s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a03s-user \
+    omni_a03s-userdebug \
+    omni_a03s-eng
---
+++ omni_a03s.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a03s device
+$(call inherit-product, device/samsung/a03s/device.mk)
+
+PRODUCT_DEVICE := a03s
+PRODUCT_NAME := omni_a03s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A037F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a03snnxx-user 12 SP1A.210812.016 A037FXXU4CWG6 release-keys"
+
+BUILD_FINGERPRINT := samsung/a03snnxx/a03s:12/SP1A.210812.016/A037FXXU4CWG6:user/release-keys
